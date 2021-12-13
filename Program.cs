3fb96c501ea57e8d41252522b801e6a6781db21a
stheisis22 <44663142+stheisis22@users.noreply.github.com>
2021-12-13 00:46:11 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISBN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. ISBN 10");
+            Console.WriteLine("2. ISBN 13");
+            int opt = int.Parse(Console.ReadLine());
+
+            switch (opt)
+            {
+                case (1):
+                    Console.WriteLine("Validacion formato 10;");
+
+                    int ISBN10;
+                    int[] digit = new int[10];
+
+                    for (int i = 0; i < 10; i++)
+                    {
+                        digit[i] = int.Parse(Console.ReadLine());
+                        if(digit[i]>9 || digit[i]<0) 
+                        {
+                            Console.WriteLine("Valor invalido");
+                        }
+                    }
+                    for (int j = 0; j < 10; j++)
+                    {
+                        Console.WriteLine(digit[j]);
+                    }
+
+                    ISBN10 = ((digit[0] * 10) + (digit[1] * 9) + (digit[2] * 8) + (digit[3] * 7) + (digit[4] * 6) + (digit[5] * 5) + (digit[6] * 4) + (digit[7] * 3) + (digit[8] * 2) + (digit[9] * 1) %11);
+
+                    if (ISBN10 !=0)
+                    {
+                        Console.WriteLine("ISBN invalidado");
+                    }
+                    else if (ISBN10 == 0)
+                    {
+                        Console.WriteLine("ISBN valido");
+                    }
+                    Console.ReadKey();
+                    break;
+
+                case (2):
+                    Console.WriteLine("Validacion formato 13;");
+                    int ISBN13;
+                    int[] digit13 = new int[13];
+
+                    for (int i = 0; i < 13; i++)
+                    {
+                        digit13[i] = int.Parse(Console.ReadLine());
+                        if (digit13[i] >9 || digit13[i]<0)
+                        {
+                            Console.WriteLine("Valor invalidado");
+                        }
+                    }
+                    for (int j = 0; j < 13; j++)
+                    {
+                        Console.WriteLine(digit13[j]);
+                    }
+
+                    ISBN13 = ((digit13[0] * 1) + (digit13[1] * 3) + (digit13[2] * 1) + (digit13[3] * 3) + (digit13[4] * 1) + (digit13[5] * 3) + (digit13[6] * 1) + (digit13[7] * 3) + (digit13[8] * 1) + (digit13[9] * 3) + (digit13[10] * 1) + (digit13[11] * 3) + (digit13[12] * 1) %10);
+
+                    if (ISBN13 !=0)
+                    {
+                        Console.WriteLine("ISBN invalidado");
+                    }
+                    else if (ISBN13 ==0)
+                    {
+                        Console.WriteLine("ISBN valido");
+                    }
+                    Console.ReadKey();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject3
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodISBN10Valid()
+        {
+            int ISBN10 = 0;
+            bool ISBNvalid = false;
+            int[] digit = { 0, 7, 1, 6, 7, 0, 3, 4, 4, 0 };
+
+            ISBN10 = ((digit[0] * 10) + (digit[1] * 9) + (digit[2] * 8) + (digit[3] * 7) + (digit[4] * 6) + (digit[5] * 5) + (digit[6] * 4) + (digit[7] * 3) + (digit[8] * 2) + (digit[9] * 1));
+            ISBNvalid = (ISBN10 % 11 == 0);
+
+            if (ISBN10 == 0)
+            {
+                Assert.IsTrue(ISBNvalid);
+            }
+        }
+
+        [TestMethod]
+        public void TestMethodISBN10Invalid()
+        {
+            int ISBN10 = 0;
+            bool ISBNinvalid = false;
+            int[] digit = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 1 };
+
+            ISBN10 = ((digit[0] * 10) + (digit[1] * 9) + (digit[2] * 8) + (digit[3] * 7) + (digit[4] * 6) + (digit[5] * 5) + (digit[6] * 4) + (digit[7] * 3) + (digit[8] * 2) + (digit[9] * 1));
+            ISBNinvalid = (ISBN10 % 11 == 0);
+
+            if (ISBN10 != 0)
+            {
+                Assert.IsFalse(ISBNinvalid);
+            }
+        }
+
+        [TestMethod]
+        public void TestMethodISBN13valid()
+        {
+            int ISBN13 = 0;
+            bool ISBNvalid13 = false;
+            int[] digit = { 9, 7, 8, 0, 7, 1, 6, 7, 0, 3, 4, 4, 0 };
+
+            ISBN13 = ((digit[0] * 1) + (digit[1] * 3) + (digit[2] * 1) + (digit[3] * 3) + (digit[4] * 1) + (digit[5] * 3) + (digit[6] * 1) + (digit[7] * 3) + (digit[8] * 1) + (digit[9] * 3) + (digit[10] * 1) + (digit[11] * 3) + (digit[12] * 1));
+            ISBNvalid13 = (ISBN13 % 10 == 0);
+
+            if (ISBN13 == 0)
+            {
+                Assert.IsTrue(ISBNvalid13);
+            }
+        }
+
+        [TestMethod]
+        public void TestMethodISBN13INvalid()
+        {
+            int ISBN13 = 0;
+            bool ISBNvalid13 = false;
+            int[] digit = {1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 0 };
+
+            ISBN13 = ((digit[0] * 1) + (digit[1] * 3) + (digit[2] * 1) + (digit[3] * 3) + (digit[4] * 1) + (digit[5] * 3) + (digit[6] * 1) + (digit[7] * 3) + (digit[8] * 1) + (digit[9] * 3) + (digit[10] * 1) + (digit[11] * 3) + (digit[12] * 1));
+            ISBNvalid13 = (ISBN13 % 10 == 0);
+
+            if (ISBN13 != 0)
+            {
+                Assert.IsFalse(ISBNvalid13);
+            }
+        }
+
+    }
+}
